1c8255096bc4e5d36c036e9e83425c17da73bf6c
Ricardo Presilla <ricardopresilla@gmail.com>
2022-12-08 10:36:03 -0300

    Update dacumentation and add main.

---
+++ src/main/java/com/alfaCentauri/ConvertirXLSToCSV.java
@@ -0,0 +1,13 @@
+package com.alfaCentauri;
+
+public class ConvertirXLSToCSV {
+
+    /**
+     * @param args Type array String.
+     **/
+    public static void main(String []args){
+        System.out.println("Ejemplo en Java para convertir un documento de Excel a CSV con la librería Apache POI.");
+
+        System.out.println("Fin del programa.");
+    }
+}
